87a214b8e2063e6ee24a3683cceba64dc689a465
Nicholas <nicholas.tiongkh@shopee.com>
2023-03-28 23:49:20 +0800

    feat: add three.js

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter'
+import { Camera, Material, Texture } from 'three';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
